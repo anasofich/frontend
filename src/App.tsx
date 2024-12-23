e384c2e1a963e888a7cd5d63d26fc50f1627a94e
Ana Sofia Castellanos <castellanos_chopas@hotmail.com>
2024-12-23 00:09:25 -0600

    calendar first version

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "./styles/overviewTab/navBar.css";
 import "./styles/overviewTab/modal.css";
 import "./styles/overviewTab/overview.css";
 import "./styles/overviewTab/activitiesModal.css";
+import "./styles/calendarTab/calendar.css";
+import "./styles/calendarTab/addActivityModal.css";
 
 function App() {
   return <MainRoutes />;
