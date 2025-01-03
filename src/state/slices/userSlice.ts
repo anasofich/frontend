5f3df6bc21368b2d79cb518b46831f8cd52cd514
Ana Sofia Castellanos <castellanos_chopas@hotmail.com>
2025-01-03 12:08:14 -0600

    corrected some things

--- src/state/slices/userSlice.ts
+++ src/state/slices/userSlice.ts
@@ -1,4 +1,4 @@
-import { PayloadAction, createSlice, Reducer } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
 interface PersistPartial {
